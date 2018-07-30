c8b7bb2d64af413a1949ab2f5e3ddd39d6f0e517
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 09:46:44 +0200

    exo1p6 php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <?php
+      echo $_GET['lastname'] . ' ' . $_GET['firstname'];
+    ?>
+  </body>
+</html>
